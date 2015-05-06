a862ecbef5dae21ef860a46523f5a85ddc3335fd
David Raul Gonzalez <MoUsTcheMan@hotmail.com>
2015-05-05 18:10:06 -0600

    Added LinkedListNode

---
+++ Design/src/persistent/collections/LinkedList/LinkedListNode.java
@@ -0,0 +1,32 @@
+package linked_list;
+
+public class LinkedListNode<T extends Persistable> implements Persistable {
+	protected long previous;
+	protected long next;
+	protected T value;
+	
+	public LinkedListNode(T val) {
+		this.value = val;
+		this.previous = -1;
+		this.next = -1;
+	}
+	
+	@Override
+	public int getSize() {
+		return (Long.BYTES * 2) + value.getSize();
+	}
+	
+	@Override
+	public void serialize(ByteBuffer buffer) {
+		buffer.putLong(previous);
+		buffer.putLong(next);
+		value.serialize(buffer);
+	}
+	
+	@Override
+	public void load(ByteBuffer buffer) {
+		this.previous = buffer.getLong();
+		this.next = buffer.getLong();
+		this.value.load(buffer);
+	}
+}
